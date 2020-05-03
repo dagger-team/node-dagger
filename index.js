c1d35b00498684309fb85ee533d9fcf257aa2939
Michael Lyons <mdl0394@gmail.com>
2020-05-03 15:26:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const path = require('path');
+
+const fetch = require('node-fetch');
+
+function _isPromise(obj) {
+    return obj && obj.then && typeof obj.then === 'function';
+}
+
+const sendTaskStatus = async (status, task_name, id, input, output, metadata) => {
+    const body = {
+        status,
+        task_name,
+        id,
+        input,
+        output,
+        metadata
+    };
+
+    await fetch('https://api.getdagger.com/v1/tasks/status', {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    });
+};
+
+const monkeypatchLambdaHandler = (handler_path) => {
+    // lewl I did it
+    const Runtime = require.cache['/var/runtime/Runtime.js'].exports;
+    const previous_handleOnce = Runtime.prototype.handleOnce;
+    const new_handleOnce = function() {
+        const previous_handler = this.handler;
+        const new_handler = async function(event, context, callback) {
+            let return_value;
+
+            const name = context.functionName;
+            const id = context.awsRequestId;
+            const metadata = {
+                function_version: context.functionVersion,
+                function_name: context.functionName,
+                memoryLimitInMB: context.memoryLimitInMB,
+                logGroupName: context.logGroupName,
+                logStreamName: context.logStreamName,
+                invokedFunctionArn: context.invokedFunctionArn,
+                awsRequestId: context.awsRequestId
+            };
+            const input = event;
+
+            await sendTaskStatus('started', name, id, input, {}, metadata);
+
+            try {
+                return_value = previous_handler.apply(this, arguments);
+
+                if(_isPromise(return_value)) {
+                    return_value = await return_value;
+                }
+
+                if(typeof return_value !== 'object') {
+                    return_value = {return_value};
+                }
+
+                await sendTaskStatus('succeeded', name, id, input, return_value, metadata);
+            } catch(error) {
+                const error_object = {
+                    name: error.name,
+                    message: error.message,
+                    stack: error.stack
+                };
+                await sendTaskStatus('failed', name, id, input, error_object, metadata);
+
+                throw error;
+            }
+
+            return return_value;
+        };
+        this.handler = new_handler;
+
+        return previous_handleOnce.apply(this, arguments);
+    };
+    Runtime.prototype.handleOnce = new_handleOnce;
+};
+
+// Lambda Runtime uses setImmediete, so we use nextTick, which runs before that
+process.nextTick(() => {
+    try {
+        monkeypatchLambdaHandler(process.env._HANDLER);
+    } catch(error) {
+        console.log(error);
+    }
+});
